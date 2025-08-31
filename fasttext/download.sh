570d142e223caf0205eb831719d23ee431f38c00
madehua <i-madehua@stepfun.com>
2025-08-31 08:14:27 +0800

    add download github

--- fasttext/download.sh
+++ fasttext/download.sh
@@ -1,4 +1,4 @@
 group_id=${1:-0}
 source /data/anaconda3/bin/activate base
 eval $(curl -s http://deploy.i.basemind.com/httpproxy)
-python /data/code/Oprover/fasttext/download.py --threads 256 --group_id $group_id
+python /data/code/Oprover/fasttext/download.py --threads 128 --processes 64 --group_id $group_id
